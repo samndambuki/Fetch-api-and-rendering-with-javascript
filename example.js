d2b4515d25183611fa6c62dc4e141775c3184d6a
samndambuki <sam.ndambuki08@gmail.com>
2023-04-21 20:57:51 +0300

    final commit

--- example.js
+++ example.js
@@ -10,10 +10,55 @@ function fetchData(){
         return response.json()
     }).then(data => {
         console.log(data.data);
-        document.querySelector('#app').innerHTML = '<h1>Helllooo</h1>'
+        const html = data.data.map(user => {
+            return `
+            <div class="user">
+            <p><img src="${user.avatar}" alt="${user.first_name}"></p>
+            <p>Name:${user.first_name}</p>
+            <p>Email:${user.email}</p>
+            </div>`;
+           
+        }).join("");
+        // console.log(html);
+        // document.querySelector('#app').innerHTML = '<h1>Hellooo</h1>'
+        document.querySelector("#app").insertAdjacentHTML("afterbegin",html);
     }).catch(error=>{
         console.log(error);
     });
 }
 
 fetchData();
+
+
+function postData(){
+    fetch('https://reqres.in/api/users',{
+        //options to fetch
+        method:'POST',
+        headers:{
+            //what to post
+            'Content-type':'applcation/json'
+        },
+        //turns it to json
+        body:JSON.stringify({
+            name:"morpheus",
+            job:"leader"
+        })
+
+    }).then(response =>{
+        // console.log(response);
+        // const data = response.json();
+        // console.log(data);
+        console.log(response);
+        if(!response.ok){
+            throw Error('ERROR');
+        }
+        return response.json()
+    }).then(data => {
+        console.log(data);
+    
+    }).catch(error=>{
+        console.log(error);
+    });
+}
+
+postData();
